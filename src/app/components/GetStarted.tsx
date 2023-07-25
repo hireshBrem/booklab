2326e9ee38c628f4d8584d278dd13fcdace68a25
Hiresh <hbremanand4@gmail.com>
2023-07-25 15:11:22 +0100

    sign

--- src/app/components/GetStarted.tsx
+++ src/app/components/GetStarted.tsx
@@ -10,7 +10,7 @@ export default function GetStarted() {
 
     return(
         <div className='text-white font-martian_mono'>
-            <Link href="/"><button className='bg-gradient-to-tr from-indigo-500 to-[#BA68C8] rounded-l-sm p-2 mt-5' onClick={async () => {await addReaderToWaitingList(email);setEmail("");console.log("func called")}}>Sign Up</button></Link>            
+            <Link href="/"><button className='bg-gradient-to-tr from-indigo-500 to-[#BA68C8] rounded-l-sm p-2 mt-5' onClick={() => {startTransition(() => addReaderToWaitingList(email));setEmail("");console.log("func called")}}>Sign Up</button></Link>            
             <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} className='rounded-r-sm h-max p-[6px] border-[3px] border-indigo-500 bg-gray-900 w-80' placeholder='Your email (for readers)' required />
         </div>
     )
