696f0d7c979904e46df5489c3dd9c93eebab2e39
Hiresh <hbremanand4@gmail.com>
2023-07-31 00:52:48 +0100

    Redeploy

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -2,7 +2,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { addUserToDB } from "@/app/actions/db_actions"
 
-export const authOptions = {
+const authOptions = {
     providers: [
         GoogleProvider({
         clientId: process.env.GOOGLE_ID || "",
