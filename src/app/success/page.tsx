bc322445683740ab8239a455e97d4e3439e005e8
Hiresh <hbremanand4@gmail.com>
2023-07-30 15:59:19 +0100

    session

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,4 +1,8 @@
-export default function Page({id}: {id:string}) {
+'use client'
+import { useSearchParams } from 'next/navigation'
+export default function Page() {
+    const searchParams = useSearchParams()
+    const id = searchParams.get('session_id')
     return (
         <div>
             <h1>Success</h1>
